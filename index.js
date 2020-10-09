bef135e4f85c8d5fee562069159db48dcbfc5a15
unknown <tranduongkyoto@gmail.com>
2020-10-09 23:48:07 +0700

    update index.html and index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function sayHello() {
-    alert("hello");
-}
+function convert(){
+    var input = document.getElementById("input").value;
+    if(input <=4 ) document.getElementById("result").innerHTML = "Điểm của bạn: F";
+    if(4.5 <= input && input <=4.9) document.getElementById("result").innerHTML = "Điểm của bạn: D";
+    if(5.0 <= input && input <=5.4) document.getElementById("result").innerHTML = "Điểm của bạn: D+";
+    if(5.5 <= input && input <=6.4) document.getElementById("result").innerHTML = "Điểm của bạn: C";
+    if(6.5 <= input && input <=6.9) document.getElementById("result").innerHTML = "Điểm của bạn: C+";
+    if(7.0 <= input && input <=7.9) document.getElementById("result").innerHTML = "Điểm của bạn: B";
+    if(8.0 <= input && input <=8.4) document.getElementById("result").innerHTML = "Điểm của bạn: B+";
+    if(8.5 <= input && input <=10) document.getElementById("result").innerHTML = "Điểm của bạn: A";
+
+}   
